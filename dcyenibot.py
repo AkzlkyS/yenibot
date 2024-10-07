2add091eda7f3ecf3aea91ebb1cb6e78e0b51b8e
AkzlkyS <arn.kizilkaya@gmail.com>
2024-10-07 15:42:38 +0300

    Update dcyenibot.py

--- dcyenibot.py
+++ dcyenibot.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 import wowoa
+import os
+import random
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -50,5 +53,56 @@ async def böl(ctx, *args):
 @bot.command()
 async def şifre(ctx,şifre_sayi=0):
     await ctx.send(wowoa.gen_pass(şifre_sayi))
+
+@bot.command()
+async def resim(ctx):
+    with open("images/bir.png","rb") as f:
+        picture=discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def meme(ctx):
+    x= random.choice(os.listdir("images"))
+    with open(f"images/{x}","rb") as f:
+        picture=discord.File(f)
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = "https://random-d.uk/api/random"
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+
+def get_fox_image_url():    
+    url = 'https://randomfox.ca/floof/'
+    res = requests.get(url)
+    data = res.json()
+    return data['image']
+
+
+@bot.command('fox')
+async def fox(ctx):
+    image_url = get_fox_image_url()
+    await ctx.send(image_url)
+
+def get_pokemon_image_url():
+    pokemon_id = random.randint(1, 898)
+    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}'
+    res = requests.get(url)
+    data = res.json()
+    return data['sprites']['front_default']
+
+
+@bot.command(name='pokemon')
+async def pokemon(ctx):
+    image_url = get_pokemon_image_url()
+    await ctx.send(image_url)
 
 bot.run("00000000000000")
